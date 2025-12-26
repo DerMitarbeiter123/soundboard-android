47fb7817c11773096ab124d6ef48bcca7ec1e46e
Your Name <you@example.com>
2025-12-26 19:13:27 +0100

    Add server config to allow Railway domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    strictPort: false,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+    strictPort: false,
+  },
   plugins: [
     react(),
     VitePWA({
